88d55b42c98918202dc557e43a3960446e1e8a9a
kishor neupane <kishorliv@gmail.com>
2019-12-06 00:25:58 +1100

    Move test code from linkedList.js to linkListTest.js

--- linkedList/linkedList.js
+++ linkedList/linkedList.js
@@ -34,11 +34,3 @@ class LinkedList{
         }
     };
 }
-
-
-const list = new LinkedList();
-list.add(new Node({'x':'df'}));
-list.add(new Node({'y':'dfdsf'}));
-list.add(new Node({'z':'fgfg'}));
-
-list.traverse(); // traverse
---
+++ linkedList/linkedListTest.js
@@ -0,0 +1,6 @@
+const list = new LinkedList();
+list.add(new Node({'x':'df'}));
+list.add(new Node({'y':'dfdsf'}));
+list.add(new Node({'z':'fgfg'}));
+
+list.traverse(); // traverse
