d7d80f1d287e47c2bc915cdd024876b7faba581c
kishor neupane <kishorliv@gmail.com>
2019-12-06 00:23:42 +1100

    Add Node and LinkedList classes.

---
+++ linkedList/linkedList.js
@@ -0,0 +1,44 @@
+/**
+ * Implementation of singly linked list
+ */
+
+class Node{
+    constructor(data){
+        this.next = null; // reference for node is to be pointed
+        this.data = data;
+    }
+}
+
+class LinkedList{
+    constructor(){
+        this.head = null;
+        this.rootPtr = null;
+    }
+
+    // add node at last
+    add = (node) => {
+        if(!this.rootPtr){
+            this.head = this.rootPtr = node;
+        }else{
+            this.head.next = node;
+            this.head = this.head.next;
+        }
+    };
+
+    // display whole list
+    traverse = () => {
+        this.head = this.rootPtr;
+        while(this.head){
+            console.log(this.head.data);
+            this.head = this.head.next;
+        }
+    };
+}
+
+
+const list = new LinkedList();
+list.add(new Node({'x':'df'}));
+list.add(new Node({'y':'dfdsf'}));
+list.add(new Node({'z':'fgfg'}));
+
+list.traverse(); // traverse
